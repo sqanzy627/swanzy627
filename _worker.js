f5ac83804de36428142912254c254fed729ddedd
sqanzy627 <swanzy627@gmail.com>
2025-07-18 16:04:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,10 +3,10 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "peterparkerr251.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "swanzy627"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
-const apiEmail = ""; // Ganti dengan email yang kalian gunakan
+const rootDomain = "sawnzofficial.workers.dev/sub"; // Ganti dengan domain utama kalian
+const serviceName = "lalala"; // Ganti dengan nama workers kalian
+const apiKey = "c7b5c9a65ec1e5e62332f9a5eefb6a9a951ac"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiEmail = "sawnzofficial@gmail.com"; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 let isApiReady = false;
